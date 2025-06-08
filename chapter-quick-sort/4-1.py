5f55a8340481eac80668acc31a5c7aa18b433765
SumTingWong <43645220+sumtingwoong@users.noreply.github.com>
2025-06-09 00:51:57 +0200

    Update 4-1.py

--- chapter-quick-sort/4-1.py
+++ chapter-quick-sort/4-1.py
@@ -3,15 +3,12 @@
 from random import randint
 
 
-def rec_sum(mass: list) -> int:
-    if len(mass) == 0:
+def get_sum(mass: list) -> int:
+    if not mass:
         return 0
-    elif len(mass) == 1:
-        return mass[0]
-    else:
-        return mass[0] + rec_sum(mass[1:])
+    return mass[0] + get_sum(mass[1:])
 
 
 random_list = [randint(0, 50) for i in range(randint(0, 10))]
 print(random_list)
-print(rec_sum(random_list))
+print(get_sum(random_list))
